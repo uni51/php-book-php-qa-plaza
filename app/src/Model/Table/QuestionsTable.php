a54669234084fe66eb1ecba39949ecaf588c0636
uni51 <uni51@users.noreply.github.com>
2019-03-20 10:15:30 +0900

    08-07 バリデーションの実装

--- app/src/Model/Table/QuestionsTable.php
+++ app/src/Model/Table/QuestionsTable.php
@@ -29,6 +29,27 @@ class QuestionsTable extends Table
     }
 
     /**
+     * バリデーションルールの定義
+     *
+     * @param \Cake\Validation\Validator $validator バリデーションインスタンス
+     * @return \Cake\Validation\Validator バリデーションインスタンス
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->nonNegativeInteger('id', 'IDが不正です')
+            ->allowEmpty('id', 'create', 'IDが不正です');
+
+        $validator
+            ->scalar('body', '質問内容が不正です')
+            ->requirePresence('body', 'create', '質問内容が不正です')
+            ->notEmpty('body', '質問内容は必ず入力してください')
+            ->maxLength('body', 140, '質問内容は140字以内で入力してください');
+
+        return $validator;
+    }
+
+    /**
      * 回答付きの質問一覧を取得する
      *
      * @return \Cake\ORM\Query 回答付きの質問一覧クエリ
@@ -44,5 +65,5 @@ class QuestionsTable extends Table
 
         return $query;
     }
-    
+
 }
